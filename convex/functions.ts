15a0cea196a4fe75374d1eea0c064acf46d5eed7
ace111111097897 <jaykobleal1120@gmail.com>
2025-07-14 15:11:41 -0500

    Create functions.ts

---
+++ convex/functions.ts
@@ -0,0 +1,23 @@
+import * as games from "./games";
+import * as friends from "./friends";
+import * as matches from "./matches";
+import * as profiles from "./profiles";
+import * as stories from "./stories";
+import * as storyReactions from "./storyReactions";
+import * as conversations from "./conversations";
+import * as kandi from "./kandi";
+import * as kandiQueries from "./kandiQueries";
+import * as prompts from "./prompts";
+
+export default {
+  games,
+  friends,
+  matches,
+  profiles,
+  stories,
+  storyReactions,
+  conversations,
+  kandi,
+  kandiQueries,
+  prompts,
+}; 
