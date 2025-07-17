08dbe9deae69e0ac209d4d052720acb6a47c8a9a
ace111111097897 <jaykobleal1120@gmail.com>
2025-07-17 14:48:55 -0500

    Update router.ts

--- convex/router.ts
+++ convex/router.ts
@@ -1,3 +1,27 @@
+import * as games from "./games";
+import * as friends from "./friends";
+import * as profiles from "./profiles";
+import * as conversations from "./conversations";
+import * as prompts from "./prompts";
+import * as auth from "./auth";
+import * as stories from "./stories";
+import * as storyReactions from "./storyReactions";
+import * as matches from "./matches";
+import * as emailActions from "./emailActions";
+import * as http from "./http";
+import * as ai from "./ai";
+
 export default {
-  // other modules
+  games,
+  friends,
+  profiles,
+  conversations,
+  prompts,
+  auth,
+  stories,
+  storyReactions,
+  matches,
+  emailActions,
+  http,
+  ai,
 };
