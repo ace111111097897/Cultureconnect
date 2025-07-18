da5a4b36e23d9e8253da8624a18f223190410a33
ace111111097897 <jaykobleal1120@gmail.com>
2025-07-18 15:52:57 -0500

    Update SignInForm.tsx

--- src/SignInForm.tsx
+++ src/SignInForm.tsx
@@ -80,12 +80,6 @@ export function SignInForm() {
         <span className="mx-4 text-secondary">or</span>
         <hr className="my-4 grow border-gray-200" />
       </div>
-      <button 
-        className="auth-button hover-scale" 
-        onClick={() => void signIn("anonymous")}
-      >
-        Sign in anonymously
-      </button>
     </div>
   );
 }
